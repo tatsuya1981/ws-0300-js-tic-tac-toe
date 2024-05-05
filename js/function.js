e5a2c7fb7d968eb87f4c4c9722f2e39c15c9a267
tatsuya <clarkmaster@icloud.com>
2024-05-05 13:36:48 +0900

    modified: js/function.js

--- js/function.js
+++ js/function.js
@@ -10,10 +10,12 @@ const winningPatterns = [
 ];
 const stateMessageElement = document.querySelector(".js-state-message");
 const cells = document.querySelectorAll(".js-cell");
+const restart = document.querySelector(".js-restart");
 let currentPlayer = "circle";
 let board = Array(9).fill(null);
+let done = false;
+let num = 0;
 
-updateTurnDisplay();
 cells.forEach((cell) => {
   cell.addEventListener("click", handleCellClick);
 });
@@ -21,21 +23,28 @@ cells.forEach((cell) => {
 function handleCellClick(event) {
   const cell = event.target;
   const index = parseInt(cell.dataset.key, 10) - 1;
-
-  if (board[index] !== null || checkWinner()) return;
+  if (board[index] !== null || done) return;
   board[index] = currentPlayer;
   cell.textContent = currentPlayer === "circle" ? "○" : "×";
-
-  if (checkWinner()) {
-  } else if (board.every((cell) => cell !== null)) {
-    stateMessageElement.textContent = "draw";
+  if (checkWinner(board, winningPatterns)) {
+    stateMessageElement.textContent = `${
+      currentPlayer === "circle" ? "○" : "×"
+    } win!!`;
+    done = true;
+    removeClickListener();
   } else {
+    num++;
     currentPlayer = currentPlayer === "circle" ? "cross" : "circle";
-    updateTurnDisplay();
+    updateTurnDisplay(num);
   }
 }
 
-function updateTurnDisplay() {
+function updateTurnDisplay(num) {
+  if (num === 9) {
+    stateMessageElement.textContent = "draw";
+    done = true;
+    removeClickListener();
+  }
   const turnItems = document.querySelectorAll(".turn-item");
   turnItems.forEach((item) => {
     item.classList.remove("active");
@@ -45,14 +54,33 @@ function updateTurnDisplay() {
   });
 }
 
-function checkWinner() {
+function checkWinner(cells, winningPatterns) {
   return winningPatterns.some((pattern) => {
-    if (pattern.every((index) => board[index] === currentPlayer)) {
-      stateMessageElement.textContent = `${
-        currentPlayer === "circle" ? "○" : "×"
-      } win!!`;
-      return true;
-    }
-    return false;
+    const [first, second, third] = pattern;
+    return (
+      cells[first] !== null &&
+      cells[first] === cells[second] &&
+      cells[second] === cells[third]
+    );
   });
 }
+
+function removeClickListener() {
+  cells.forEach((cell) => cell.removeEventListener("click", handleCellClick));
+}
+
+restart.addEventListener("click", restartGame);
+
+function restartGame() {
+  location.reload();
+}
+
+restart.addEventListener("mouseover", function () {
+  restart.style.backgroundColor = "#000000";
+  restart.style.color = "#fefefe";
+});
+
+restart.addEventListener("mouseleave", function () {
+  restart.style.backgroundColor = "#fefefe";
+  restart.style.color = "#000000";
+});
